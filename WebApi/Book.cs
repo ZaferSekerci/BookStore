b5f8d74f8f547777258e30f635bb9a6ba961c11d
ZaferSekerci <zafersekerci@ya.ru>
2022-01-10 21:14:51 +0300

    auto increment id column added

--- WebApi/Book.cs
+++ WebApi/Book.cs
@@ -1,16 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApi
 {
     public class Book
     {
-        public int Id{get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
         public string Title { get; set; }
- 
-        public int  GenreId { get; set; }
 
-        public int PageCount {get; set;}
+        public int GenreId { get; set; }
+
+        public int PageCount { get; set; }
 
         public DateTime PublishDate { get; set; }
 
